dd1140212cb1b24ed62beaa310c0a39517289c31
mrlldd <roflanjaraxxus@gmail.com>
2023-07-25 22:31:01 +0300

    use promises of fs lib

--- scripts/rename-solution.js
+++ scripts/rename-solution.js
@@ -1,25 +1,40 @@
 const { glob } = require('glob');
-const fs = require('fs');
+const { promises: fs } = require('fs');
 const replace = require('replace');
 
 const currentSolutionName = 'RichWebApi';
+function validateSolutionName(name) {
+	const notAllowedCharacters = ['\'', '"', '/', '\\', '?', ':', '&', '*', '<', '>', '|', '#', '%', ' '];
+	if (typeof name !== 'string') {
+		throw new Error('Solution name should be a string');
+	}
+	if (name === '.' || name === '..') {
+		throw new Error('Solution name can\'t be a dot');
+	}
+	if (notAllowedCharacters.some((c) => name.includes(c))) {
+		throw new Error(`Solution name can't contain space or not allowed characters: ${notAllowedCharacters}`);
+	}
+}
 
 (async function main() {
 	const replaceSolutionNameFor = process.argv.slice(2)[0];
 	if (!replaceSolutionNameFor) {
 		throw new Error('Missing target solution name');
 	}
-	validateTargetSolutionName(replaceSolutionNameFor);
+	validateSolutionName(replaceSolutionNameFor);
 
+	if (replaceSolutionNameFor === currentSolutionName) {
+		console.info('Current solution name is the same as target name');
+		return;
+	}
 	const files = await glob('./../**/*', {
 		dot: false,
-		ignore: ['../node_modules/**', '../LICENSE', '../CODEOWNERS', '../README.md', '**/bin', '**/obj']
+		ignore: ['../node_modules/**', '../LICENSE', '../CODEOWNERS', '../README.md']
 	});
 
-	console.log('Found files to replace old solution name occurrences:', files);
+	console.info('Found files to replace old solution name occurrences:', files);
 	// Replace case-sensitive occurrences
 	replace({
-		recursive: true,
 		regex: `${currentSolutionName}`,
 		replacement: replaceSolutionNameFor,
 		paths: files,
@@ -27,7 +42,6 @@ const currentSolutionName = 'RichWebApi';
 	});
 	//Replace lowercase occurrences
 	replace({
-		recursive: true,
 		regex: `${currentSolutionName.toLowerCase()}`,
 		replacement: replaceSolutionNameFor.toLowerCase(),
 		paths: files,
@@ -45,29 +59,15 @@ const currentSolutionName = 'RichWebApi';
 		return countSlashes(y.path) > countSlashes(x.path) ? 1 : -1;
 	});
 
-	console.log('Found files to rename:', filesToRename.map(x => x.path));
-	filesToRename.forEach((x) => {
-		const targetName =
+	console.info('Found files to rename:', filesToRename.map(x => x.path));
+	const promises = filesToRename.map(async (x) => {
+		const targetPath =
 			x.path.substring(0, x.lastSlashIndex) +
 			x.path.substring(x.lastSlashIndex).replace(currentSolutionName, replaceSolutionNameFor);
-		fs.rename(x.path, targetName, (error) => {
-			if (error) {
-				throw error;
-			}
-			console.log('Renamed', x.path, 'to', targetName);
-		});
-	});
-})();
 
-function validateTargetSolutionName(name) {
-	const notAllowedCharacters = ['\'', '"', '/', '\\', '?', ':', '&', '*', '<', '>', '|', '#', '%', ' '];
-	if (typeof name !== 'string') {
-		throw new Error('Target solution name should be a string');
-	}
-	if (name === '.' || name === '..') {
-		throw new Error('Target solution name can\'t be a dot');
-	}
-	if (notAllowedCharacters.some((c) => name.includes(c))) {
-		throw new Error(`Target solution name can't contain space or not allowed characters: ${notAllowedCharacters}`);
-	}
-}
+		await fs.rename(x.path, targetPath);
+		console.log('Renamed', x.path, 'to', targetPath);
+	});
+	await Promise.all(promises);
+})
+();
