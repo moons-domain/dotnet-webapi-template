89957aa821369a68ad7ba1566f2f867cc6634f44
mamay12 <ytimchenko57@gmail.com>
2023-08-02 15:54:57 +0300

    add other templates

---
+++ vs2022/templates/Rich.AsyncStartupAction/AsyncStartupAction.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace $rootnamespace$;
+
+[UsedImplicitly]
+internal sealed class $safeitemname$ : IAsyncStartupAction
+{
+	public uint Order => 0;
+
+	public $safeitemname$()
+	{
+	}
+
+	public Task PerformActionAsync(CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ vs2022/templates/Rich.MappingProfile/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using JetBrains.Annotations;
+
+namespace $rootnamespace$;
+
+[UsedImplicitly]
+public class $safeitemname$ : Profile 
+{
+    public $safeitemname$() 
+    {
+    }
+}
