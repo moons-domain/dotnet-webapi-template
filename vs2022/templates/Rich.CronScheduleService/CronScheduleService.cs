ee87205c7f622058a21aebd4b18da711a10cf2dd
mamay12 <ytimchenko57@gmail.com>
2023-08-02 15:43:37 +0300

    add first templates for vs2022

---
+++ vs2022/templates/Rich.CronScheduleService/CronScheduleService.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using NCrontab;
+
+namespace $rootnamespace$;
+
+[UsedImplicitly]
+internal sealed class $safeitemname$ : CronScheduleService
+{
+    public $safeitemname$(IServiceProvider serviceProvider) : base(serviceProvider)
+	{
+}
+
+    public override Task PerformServiceFunctionAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override CrontabSchedule GetSchedule()
+    {
+        throw new NotImplementedException();
+    }
+
+}
